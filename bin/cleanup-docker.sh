4837939ebdd86d85090d74f648be87af942e089c
Ricky Chiang <ricky.chiang@quantatw.com>
2017-04-13 09:05:08 +0800

    Update cleanup-docker.sh, for Version 1.13 up

--- bin/cleanup-docker.sh
+++ bin/cleanup-docker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-docker rm `docker ps -q -f status=exited` 
-docker rmi $(docker images -q -f "dangling=true")
+#docker ps -aqf status=exited | xargs docker rm
+#docker images -qf dangling=true | xargs docker rmi
+
+# Remove Unused Containers
+docker container prune -f
+
+# cleanup images
+docker image prune -f
+
+# cleanup unused volume
+docker volume prune -f
