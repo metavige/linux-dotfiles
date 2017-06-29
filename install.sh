9edd68fd3211ab470b22ed0090f9ef94358245c9
Ricky Chiang <ricky.chiang@quantatw.com>
2017-06-29 11:21:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ cd ~
 git clone http://192.168.0.22/99122010/dotfiles && \
     cd dotfiles && ./sync.sh -f 
 cd ~ && rm -rf dotfiles
-sudo locale-gen zh_TW zh_TW.UTF-8
+# sudo locale-gen zh_TW zh_TW.UTF-8
