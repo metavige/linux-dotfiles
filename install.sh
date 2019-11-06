1a0cd643fa5b7ff5c392703debae61acc3d34dcd
metavige <metavige@gmail.com>
2019-11-06 09:11:22 +0800

    update sync process

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 cd ~ 
 git clone http://192.168.0.22/99122010/dotfiles.git && \
-    cd dotfiles && sh ./sync.sh -f 
+    cd dotfiles && \
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
+      --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+    source ~/.bash_profile;
+ 
 cd ~ && rm -rf dotfiles
 # sudo locale-gen zh_TW zh_TW.UTF-8
