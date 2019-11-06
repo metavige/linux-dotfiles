0a8780ab2f1fb8570cd9082828f8c181238ea5a5
metavige <metavige@gmail.com>
2019-11-06 09:05:44 +0800

    change mode

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 cd ~ 
 git clone http://192.168.0.22/99122010/dotfiles.git && \
-    cd dotfiles && ./sync.sh -f 
+    cd dotfiles && sh ./sync.sh -f 
 cd ~ && rm -rf dotfiles
 # sudo locale-gen zh_TW zh_TW.UTF-8
