8905318303a6b3840c8ca6104a2b7b12ece8a8f7
metavige <metavige@gmail.com>
2020-02-24 14:44:28 +0800

    Update git clone folder

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ~ 
-git clone https://github.com/metavige/linux-dotfiles.git && \
+git clone https://github.com/metavige/linux-dotfiles.git dotfiles && \
     cd dotfiles && \
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
       --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
